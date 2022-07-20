ab2185f1921ccb6b3772b9d2d436b693c32cf8da
Brien Thomas <thomasbrien94@gmail.com>
2022-07-20 14:45:40 -0700

    added fetch functions to create user profiles, add to liked_pets table in supabase, delete from from liked_pets table.

--- src/Components/AnimalsList.js
+++ src/Components/AnimalsList.js
@@ -1,28 +1,58 @@
-import React from 'react';
-// import { useDataContext } from '../DataProvider';
+import React, { useEffect } from 'react';
+import { useDataContext } from '../DataProvider';
 
 export default function AnimalsList({ animals }) {
-  // const {} = useDataContext();
+  const { handleAddToLikedList,
+    handleDeleteFromLikedList,
+    handleFetchFromLikedList,
+    likedList, } = useDataContext();
+
+  useEffect(() => {
+    if (!likedList) handleFetchFromLikedList();
+
+  }, []); //eslint-disable-line
+
   if (!animals) return null;
   return (
     <div className="animals-list">
       
-      {animals.map((animal) => (
-        <div key={animal.url + animal.descriptions}>
-          {animal.photos[0]?.full && (<div className="animal-card" 
-            key={animal.id + animal.url}>
-            <h2>{animal.name}</h2>
-            <img src={animal.photos[0].full} />
+      {animals.map((animal) => {
+        const alreadyOnLikedList = likedList && likedList.find((likedList) =>
+          likedList.id === animal.id);
+        return (
+          <div key={animal.url + animal.descriptions}>
+            {animal.photos[0]?.full && (<div className="animal-card" 
+              key={animal.id + animal.url}>
+              <h2>{animal.name}</h2>
+              <img src={animal.photos[0].full} />
             
-            <p><b>{animal.name}</b> {animal.description}</p>
-            <button>Add to Liked</button>
+              <p><b>{animal.name}</b> {animal.description}</p>
+              <button className='material-symbols-outlined' onClick={() => 
+                alreadyOnLikedList
+                  ? handleDeleteFromLikedList(alreadyOnLikedList.id)
+                  : handleAddToLikedList({
+                    id: animal.id,
+                    name: animal.name,
+                    photo: animal.photos[0].full,
+                    type: animal.type,
+                    description: animal.description,
+                    age: animal.age,
+                    breed: animal.breed,
+                  })
+              }> favorite </button>
+            </div>
+            )}
           </div>
-          )}
-        </div>
         
+//         <button>
+//               <span className="material-symbols-outlined">
+// favorite
+//               </span>
+//             </button>
 
         
-      ))}
+        );})
+      }
       Certified by: Julie N. (tm)
     </div>
   );
